64a6a309d811a33764f7a491a40176e345deb479
Fabian_Omoke <f.ombui@blue-planet-ecosystems.com>
2023-08-25 13:30:17 +0200

    Blog search implemented

--- blog_manager/mysite/blog/models.py
+++ blog_manager/mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -11,6 +12,7 @@ class PublishedManager(models.Manager):
 # Create your models here.
 class Post(models.Model):
 
+    tags = TaggableManager()
     objects = models.Manager() # The default manager
     published = PublishedManager() # Our custom manager
     
--- blog_manager/mysite/blog/views.py
+++ blog_manager/mysite/blog/views.py
@@ -3,9 +3,12 @@ from .models import Post, Comment
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count 
+from django.contrib.postgres.search import SearchVector, SearchRank,SearchQuery
 
 @require_POST
 def post_comment(request, post_id):
@@ -42,8 +45,13 @@ class PostListView(ListView):
 
 
 # Create your views here.
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     # pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get(
@@ -59,7 +67,7 @@ def post_list(request):
         # if page_number is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
     return render(
-        request, 'blog/post/list.html', {'posts': posts} )
+        request, 'blog/post/list.html', {'posts': posts, 'tag': tag} )
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
@@ -75,11 +83,19 @@ def post_detail(request, year, month, day, post):
     # Form for users to comment
     form = CommentForm()
 
+    # list of similar posts based on tags
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+    .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+    .order_by('-same_tags','-publish')[:4]
+
     return render (request,
                    "blog/post/detail.html", 
                    {"post": post,
                    'comments': comments,
-                   'form':form})
+                   'form':form,
+                   'similar_posts' : similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
@@ -101,5 +117,24 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
                   'form': form,
-                  'sent': sent })
+                  'sent': sent 
+                  })
     
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        # search by title or content
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search = search_vector, rank=SearchRank(search_vector, search_query)).filter(
+                search=search_query).order_by('-rank')
+    return render(
+        request,'blog/post/search.html',{'form':form,"query" : query ,"results" : results }
+    )
